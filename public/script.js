3658fdfbe2b4b72572664823f809a5d61d0306ee
Kevin <kevinbandeira2006@gmail.com>
2024-01-25 13:12:38 -0300

    FINAL DO FINAL

--- public/script.js
+++ public/script.js
@@ -45,3 +45,55 @@ currentSlide = currentSlide - 1
 
 
 setInterval(nextSlider, 3000)
+
+
+
+const slidermob = document.querySelectorAll('.mob-slider');
+const btnPrevmob = document.getElementById('mobp-button');
+const btnNextmob = document.getElementById('mobn-button');
+
+let currentSlidemob = 0;
+
+function hideSlidermob() {
+    slidermob.forEach(item => item.classList.remove('on'))
+}
+
+function showSlidermob() {
+    slidermob[currentSlidemob].classList.add('on')
+}
+
+function nextSlidermob() {
+    hideSlidermob()
+    if (currentSlidemob === slidermob.length -1) {
+        currentSlidemob = 0
+    }
+    else {
+        currentSlidemob++
+    }
+    showSlidermob()
+}
+
+function prevSlidermob() {
+    hideSlidermob()
+    if (currentSlidemob === 0) {
+        currentSlidemob = slidermob.length -1
+    }
+    else {
+        currentSlidemob--
+    }
+    showSlidermob()
+}
+
+
+btnNextmob.addEventListener('click', nextSlidermob)
+btnPrevmob.addEventListener('click', prevSlidermob)
+
+console.log(slidermob)
+
+currentSlidemob = currentSlidemob + 1
+currentSlidemob = currentSlidemob - 1
+
+
+setInterval(nextSlidermob, 3000)
+
+
